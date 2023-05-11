4dab4f6c14790bc1b6f719d0b8688da6815cd8ec
onurural <54706493+onurural@users.noreply.github.com>
2023-05-11 14:12:33 +0300

    Initial Commit

---
+++ AppcentCaseStudy/AppcentCaseStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppcentCaseStudyApp.swift
+//  AppcentCaseStudy
+//
+//  Created by Onur Ural on 11.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppcentCaseStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
